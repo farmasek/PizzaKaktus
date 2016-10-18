33d3496ac745076e24ce598a21d03b54c754bec1
Vojtěch Baránek <vojtabaranek@gmail.com>
2016-10-18 21:59:57 +0200

    Backend project init

---
+++ src/main/java/cz/osu/pizzakaktus/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cz.osu.pizzakaktus;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PizzaKaktusApplication.class);
+	}
+
+}
