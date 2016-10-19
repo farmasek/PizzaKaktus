80da32e7f7c60a6a9223af6eba75c4ea2dbda75c
Vojtech Baranek <vojtech.baranek@tieto.com>
2016-10-19 11:12:02 +0200

    Ingredient crud - backend

---
+++ source/backend/src/main/java/cz/osu/pizzakaktus/services/IngredientService.java
@@ -0,0 +1,26 @@
+package cz.osu.pizzakaktus.services;
+
+import cz.osu.pizzakaktus.repositories.models.IngredientDb;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by baranvoj on 19.10.2016.
+ */
+public interface IngredientService {
+
+    /**
+     * Insert ingredient into database
+     * @param ingredientDb - ingredient to be put in database
+     * @return inserted ingredient
+     */
+    Optional<IngredientDb> insert(IngredientDb ingredientDb);
+
+    /**
+     * Find all ingredients in database
+     * @return list of ingredients
+     */
+    List<IngredientDb> findAll();
+
+}
