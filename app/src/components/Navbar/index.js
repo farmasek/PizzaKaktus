20979f8fbdae97780a78be023b067fce23412ec8
Vojtěch Baránek <vojtabaranek@gmail.com>
2016-10-22 00:43:31 +0200

    Frontend project init

---
+++ app/src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './index.module.scss';
+import cssModules from 'react-css-modules';
+import Navigation from 'react-toolbox/lib/navigation';
+import { Link } from 'react-router';
+
+const Navbar = () => (
+  <div>
+    <Navigation type="horizontal">
+      <Link to="#">First page</Link>
+      <Link to="#">Second page</Link>
+    </Navigation>
+  </div>
+);
+
+export default cssModules(Navbar, styles);
