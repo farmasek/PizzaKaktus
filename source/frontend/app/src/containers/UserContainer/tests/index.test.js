9c77281b9b050f6132a54658ce5e7178e41b64c6
GulaShek <matej.nahly@seznam.cz>
2016-11-20 12:50:02 +0100

    Testy pro juzra

--- source/frontend/app/src/containers/UserContainer/tests/index.test.js
+++ source/frontend/app/src/containers/UserContainer/tests/index.test.js
@@ -1,11 +1,63 @@
-// import User from '../index';
-//
-// import expect from 'expect';
-// import { shallow } from 'enzyme';
-// import React from 'react';
-//
-// describe('<User />', () => {
-//   it('Expect to have unit tests specified', () => {
-//     expect(true).toEqual(false);
-//   });
-// });
+import UserContainer from '../index';
+import UserList from '../../../components/UserList'
+import { Provider } from 'react-redux';
+import { expect } from 'chai';
+import React from 'react';
+import thunk from 'redux-thunk';
+import { mount, shallow } from 'enzyme';
+import { List, fromJS } from 'immutable'
+import events from 'events';
+
+import configureMockStore from 'redux-mock-store';
+
+
+const mockStore = configureMockStore([thunk]);
+
+
+describe('USER CONTAINER', () => {
+
+  let store, component;
+
+  const storeToMock = {
+    userContainer: {
+      loading: false,
+      users: new List(),
+      userForm: fromJS({
+        firstName: '',
+        lastName: '',
+        password: '',
+        login: '',
+        roles: [],
+        phone: '',
+      }),
+    }
+  }
+  const storeToMockIsLoading = {
+    userContainer: {
+      loading: false,
+      users: new List(),
+      userForm: fromJS({
+        firstName: '',
+        lastName: '',
+        password: '',
+        login: '',
+        roles: [],
+        phone: '',
+      }),
+    }
+  }
+
+
+  it('should render User list if not loading', () => {
+    store = mockStore(storeToMock);
+    component = shallow(<UserContainer store={store}/>).shallow();
+    expect(component.contains(<UserList users={new List()}/>)).to.be.equal(true);
+  });
+
+  it('should not render User list if loading', () => {
+    store = mockStore(storeToMockIsLoading);
+    component = shallow(<UserContainer store={store}/>).shallow();
+    expect(component.contains(<UserList users={new List()}/>)).to.be.equal(false);
+  });
+
+});
