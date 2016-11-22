9d3ba1303fa2c80aae943f91205174260ad76aff
Vojtěch Baránek <vojtabaranek@gmail.com>
2016-11-22 23:03:47 +0100

    Deploy stuff
    readme, pom clean
    
    
    deploy adresss fix
    
    
    Deploz settting frontend
    
    
    prod path
    
    
    deplset2
    
    
    backend deploy settings

--- source/backend/src/main/resources/data.sql
+++ source/backend/src/main/resources/data.sql
@@ -1,17 +1,17 @@
-insert into role (role) VALUES ("ADMIN");
-insert into role (role) VALUES ("EMPLOYEE");
-
-INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("10 g", 5.00, 10.00, "tomatový základ");
-INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("10 g", 8.00, 15.00, "smetanový základ");
-INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("25 g", 15.00, 20.00, "šunka");
-INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("20 g", 15.00, 20.00, "eidam");
-INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("10 g", 20.00, 25.00, "niva");
-INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("15 g", 20.00, 25.00, "ementál");
-INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("20 g", 20.00, 25.00, "ananas");
-INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("10 g", 10.00, 15.00, "pórek");
-INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("10 g", 10.00, 15.00, "cibule");
-
-INSERT INTO category_db (`name`) VALUES ("Vegetariánské");
-INSERT INTO category_db (`name`) VALUES ("Masové");
-INSERT INTO category_db (`name`) VALUES ("Sýrové");
-INSERT INTO category_db (`name`) VALUES ("Speciální");
+insert into role (id,role) VALUES (0,'ADMIN');
+insert into role (id,role) VALUES (1,'EMPLOYEE');
+--
+-- INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("10 g", 5.00, 10.00, "tomatový základ");
+-- INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("10 g", 8.00, 15.00, "smetanový základ");
+-- INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("25 g", 15.00, 20.00, "šunka");
+-- INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("20 g", 15.00, 20.00, "eidam");
+-- INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("10 g", 20.00, 25.00, "niva");
+-- INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("15 g", 20.00, 25.00, "ementál");
+-- INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("20 g", 20.00, 25.00, "ananas");
+-- INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("10 g", 10.00, 15.00, "pórek");
+-- INSERT INTO ingredient_db (`amount`, `cost`, `cost_custom`, `name`) VALUES ("10 g", 10.00, 15.00, "cibule");
+--
+-- INSERT INTO category_db (`name`) VALUES ("Vegetariánské");
+-- INSERT INTO category_db (`name`) VALUES ("Masové");
+-- INSERT INTO category_db (`name`) VALUES ("Sýrové");
+-- INSERT INTO category_db (`name`) VALUES ("Speciální");
