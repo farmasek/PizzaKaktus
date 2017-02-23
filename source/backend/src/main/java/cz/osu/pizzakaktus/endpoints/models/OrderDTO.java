8e28daac6dd3200b7c1418ef625a60dfe2192421
Mispulin <Mish.k.a@seznam.cz>
2017-02-23 10:11:37 +0100

    Backend - OrderDTO refactoring

--- source/backend/src/main/java/cz/osu/pizzakaktus/endpoints/models/OrderDTO.java
+++ source/backend/src/main/java/cz/osu/pizzakaktus/endpoints/models/OrderDTO.java
@@ -1,10 +1,6 @@
 package cz.osu.pizzakaktus.endpoints.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import cz.osu.pizzakaktus.repositories.models.CustomerDb;
 import cz.osu.pizzakaktus.repositories.models.OrderDb;
-import cz.osu.pizzakaktus.repositories.models.OrderStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,15 +15,14 @@ import java.util.List;
 @Setter
 @Builder
 @AllArgsConstructor
-public class OrderDTO
-{
+public class OrderDTO  {
     private List<Integer> pizzasIds;
     private CustomerDTO customer;
-    private OrderStatus orderStatus;
+    private String orderStatus;
 
     public OrderDTO(OrderDb orderDb) {
         this.pizzasIds = orderDb.getPizzasIds();
         this.customer = new CustomerDTO(orderDb.getCustomer());
-        this.orderStatus = orderDb.getOrderStatus();
+        this.orderStatus = orderDb.getOrderStatus().getStatus();
     }
 }
